5bdf77286d63a37fd88764d168a4c31878278764
sujin <sujikang@ebay.com>
2021-03-22 16:04:31 +0900

    Create Initial Project

---
+++ EmojiCommit/EmojiCommit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EmojiCommit
+//
+//  Created by Kang, Su Jin (강수진) on 2021/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
