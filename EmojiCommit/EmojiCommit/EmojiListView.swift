2b7e0f3e05b4df28da9964b1829ebb215bdab8b7
sujin <sujikang@ebay.com>
2021-03-30 14:28:54 +0900

    Create EmojiListView

---
+++ EmojiCommit/EmojiCommit/EmojiListView.swift
@@ -0,0 +1,75 @@
+//
+//  EmojiListView.swift
+//  EmojiCommit
+//
+//  Created by Kang, Su Jin (강수진) on 2021/03/26.
+//
+
+import SwiftUI
+import emojidataios
+
+struct Emoji: Identifiable {
+    let id = UUID()
+    let value: String
+}
+
+struct EmojiListView: View {
+    private enum Constants {
+        static let columnCount = 5
+        static let emojiSpacing: CGFloat = 5
+        static let emojiLineSpacing: CGFloat = 10
+        static let emojiWidth: CGFloat = (UIScreen.main.bounds.size.width - CGFloat(columnCount)*emojiSpacing) / CGFloat(columnCount)
+    }
+    
+    @Binding var emojiPhase: EmojiPhase
+    @Binding var isShowingSheet: Bool
+    
+    private let emojiCategories: [EmojiCategory] = [.SMILEYS,
+                                                    .PEOPLE,
+                                                    .NATURE,
+                                                    .FOODS,
+                                                    .ACTIVITY]
+    
+    // MARK: - Grid 형태 정의
+    private var columns: [GridItem] {
+        let grids = (0..<Constants.columnCount).map { (_) in
+            GridItem(.fixed(Constants.emojiWidth), spacing:Constants.emojiSpacing)
+        }
+        return grids
+    }
+    
+    var body: some View {
+        VStack {
+            ScrollView {
+                LazyVGrid(columns: columns,
+                          spacing: Constants.emojiLineSpacing) {
+                    ForEach(emojiCategories, id: \.self) { emojiCategory in
+                        Section(header: Text(emojiCategory.rawValue).font(.title)) {
+                            
+                            let emojis = EmojiParser.emojisByCategory[emojiCategory]?.compactMap({ (emoji) -> Emoji in
+                                return Emoji(value: emoji.emoji)
+                            }) ?? []
+                            
+                            //todo string array나 Emoji: Hasable에서 id를 \.self 로 주면 에러. 왜?
+                            ForEach(emojis) { (emoji) in
+                                //todo 이모지 크게
+                                Button(emoji.value) {
+                                    emojiPhase.emoji = emoji.value
+                                    isShowingSheet = false
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }.onAppear(perform: {
+            EmojiParser.prepare()
+        })
+    }
+}
+
+struct EmojiListView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmojiListView(emojiPhase: .constant(EmojiPhase(phase: 0, emoji: "🔵")), isShowingSheet: .constant(true))
+    }
+}
--- EmojiCommit/EmojiCommit/EmojiPhaseView.swift
+++ EmojiCommit/EmojiCommit/EmojiPhaseView.swift
@@ -7,59 +7,75 @@
 
 import SwiftUI
 
-struct EmojiPhase: Hashable {
-    var phase: Int
-    var emoji: String?
+struct EmojiPhase {
+    var phase: Int = 0 //todo phase 없어도 될거같은데?
+    var emoji: String
+    
+    init(phase: Int, emoji: String) {
+        self.phase = phase
+        self.emoji = emoji
+    }
 }
 
+class EmojiPhaseViewViewModel: ObservableObject {
+    @Published var emojiPhases: [EmojiPhase]
+
+    init(emojiPhases: [EmojiPhase]){
+        self.emojiPhases = emojiPhases
+    }
+}
 
 struct EmojiPhaseRow: View {
     var emojiPhase: EmojiPhase
     var body: some View {
         HStack {
             Text("\(emojiPhase.phase) 단계")
-            Text(emojiPhase.emoji ?? "")
+            Text(emojiPhase.emoji)
             Spacer()
         }
-        
     }
 }
 
 struct EmojiPhaseView: View {
-    //todo - UserDefault 값으로 불러오기
-    @State var emojiPhases = [EmojiPhase(phase: 0, emoji: "🙆🏻‍♀️"),
-                              EmojiPhase(phase: 1, emoji: "🤛🏻"),
-                              EmojiPhase(phase: 2, emoji: "🌝"),
-                              EmojiPhase(phase: 3, emoji: "🏄🏻"),
-                              EmojiPhase(phase: 4, emoji: "⚾️")]
-    @State var isShowingSheet = false
-    @State var isNextButtonDisabled = true //todo 만약 이모지 다 있으면 false
+    //todo - 2순위 UserDefault 값으로 불러오기@@@
+    @ObservedObject var emojiPhaseViewModel: EmojiPhaseViewViewModel
+    @State private var isShowingSheet = false
+    @State private var isNextButtonEnabled = false //todo 만약 이모지 다 있으면 true
+    @State private var selectedIndex: Int?
     
     var body: some View {
         VStack {
-            Text("커밋 단계별 이모지를 선택해주세요")
-            List {
-                ForEach(emojiPhases, id: \.self) { emojiPhase in
-                    EmojiPhaseRow(emojiPhase: emojiPhase)
-                        .contentShape(Rectangle()) //make tappable include spacer
-                        .onTapGesture {
-                            isShowingSheet = true
-                        }
-                        .sheet(isPresented: $isShowingSheet) {
-                            //todo - 모달 뷰 띄우기
-                        }
+            Text("최근 선택한 index는 " + (selectedIndex?.description ?? "없습니다"))
+            List(emojiPhaseViewModel.emojiPhases.indices, id: \.self) { index in
+                EmojiPhaseRow(emojiPhase: emojiPhaseViewModel.emojiPhases[index])
+                    .contentShape(Rectangle()) //make tappable include spacer
+                    .onTapGesture {
+                        selectedIndex = index
+                        isShowingSheet = true
+                    }
+            }.sheet(isPresented: $isShowingSheet) {
+                if let selectedIndex = selectedIndex {
+                    EmojiListView(emojiPhase: $emojiPhaseViewModel.emojiPhases[selectedIndex],
+                                  isShowingSheet: $isShowingSheet)
                 }
             }
             Button("다음") {
                 //todo - 다음 동작
-            }.disabled(isNextButtonDisabled)
+            }.disabled(!isNextButtonEnabled)
+            
         }
     }
 }
 
 struct EmojiPhaseView_Previews: PreviewProvider {
+    static let phaseArray = [EmojiPhase(phase: 0, emoji: "🙆🏻‍♀️"),
+                             EmojiPhase(phase: 1, emoji: "🇶🇦"),
+                             EmojiPhase(phase: 2, emoji: "🇫🇴"),
+                             EmojiPhase(phase: 3, emoji: "🏁"),
+                             EmojiPhase(phase: 4, emoji: "🟣")]
+    
     static var previews: some View {
-        EmojiPhaseView()
+        EmojiPhaseView(emojiPhaseViewModel: EmojiPhaseViewViewModel(emojiPhases: phaseArray))
     }
 }
 
