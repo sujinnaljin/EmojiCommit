bd408c6cf6437d1a8baebf96ae007cbe1d59e72b
sujin <sujikang@ebay.com>
2021-04-06 15:18:05 +0900

    Create LoginView to get github id

---
+++ EmojiCommit/EmojiCommit/View/Commit/CommitView.swift
@@ -0,0 +1,20 @@
+//
+//  CommitView.swift
+//  EmojiCommit
+//
+//  Created by Kang, Su Jin (강수진) on 2021/04/06.
+//
+
+import SwiftUI
+
+struct CommitView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct CommitView_Previews: PreviewProvider {
+    static var previews: some View {
+        CommitView()
+    }
+}
--- EmojiCommit/EmojiCommit/View/Login/LoginView.swift
+++ EmojiCommit/EmojiCommit/View/Login/LoginView.swift
@@ -8,8 +8,32 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    //todo AppStorage 로 하면 바로 업데이트가 안된다..!
+    @AppStorage("githubId") var githubId: String = ""
+   // @State private var githubId: String = ""
+    
     var body: some View {
-        Text("login view")
+        GeometryReader { geometry in
+                VStack {
+                    Spacer()
+                    TextField("github 아이디를 입력하세요", text: $githubId)
+                        .padding()
+                        .multilineTextAlignment(.center)
+                    Spacer()
+                    
+                    //MARK: - Bottom Next Link
+                    //todo 네비게이션이 아니라 root 뷰를 바꿔야함
+                    NavigationLink(destination: CommitView(githubId: $githubId)) {
+                        BottomNextView(geometry: geometry,
+                                       isNextEnabled: !githubId.isEmpty)
+                            .navigationTitle("github 아이디 입력 👩🏻‍💻")
+                    }
+                    .disabled(githubId.isEmpty)
+                }
+                .edgesIgnoringSafeArea(.bottom)
+        }
+        
     }
 }
 
