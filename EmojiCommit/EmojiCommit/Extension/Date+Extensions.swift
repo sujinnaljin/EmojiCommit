9c0cf4fa20369ef7b979fbb6ca4d745b3c33d421
sujin <sujikang@ebay.com>
2021-04-10 20:04:29 +0900

    Create Date+Extension to extract day, month, year

---
+++ EmojiCommit/EmojiCommit/Extension/Date+Extensions.swift
@@ -0,0 +1,28 @@
+//
+//  Date+Extensions.swift
+//  EmojiCommit
+//
+//  Created by Kang, Su Jin (강수진) on 2021/04/10.
+//
+
+import Foundation
+
+extension Date {
+    public var year: Int {
+        return Calendar.current.component(.year, from: self)
+    }
+    
+    public var month: Int {
+         return Calendar.current.component(.month, from: self)
+    }
+    
+    public var day: Int {
+         return Calendar.current.component(.day, from: self)
+    }
+    
+    public var monthName: String {
+        let nameFormatter = DateFormatter()
+        nameFormatter.dateFormat = "MMMM" // format January, February, March, ...
+        return nameFormatter.string(from: self)
+    }
+}
