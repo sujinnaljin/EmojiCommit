37edc81adc38e9ab1c0228113aa26e7970406a2a
sujin <sujikang@ebay.com>
2021-04-10 20:05:49 +0900

    Create String+Extension to convert date

---
+++ EmojiCommit/EmojiCommit/Extension/String+Extensions.swift
@@ -0,0 +1,25 @@
+//
+//  String+Extensions.swift
+//  EmojiCommit
+//
+//  Created by Kang, Su Jin (강수진) on 2021/04/10.
+//
+
+import Foundation
+
+extension String {
+    func toDate() -> Date? {
+        let dateFormatter: DateFormatter = {
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "yyyy-MM-dd"
+            dateFormatter.timeZone = TimeZone(identifier: "UTC")
+            return dateFormatter
+        }()
+        
+        if let date = dateFormatter.date(from: self) {
+            return date
+        } else {
+            return nil
+        }
+    }
+}
