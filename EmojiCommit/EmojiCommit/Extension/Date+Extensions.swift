d4c9106356128b9c44665651f04d2e229451a2df
sujin <sujikang@ebay.com>
2021-05-04 16:31:42 +0900

    Change Calendar type current to gregorian

--- EmojiCommit/EmojiCommit/Extension/Date+Extensions.swift
+++ EmojiCommit/EmojiCommit/Extension/Date+Extensions.swift
@@ -9,19 +9,19 @@ import Foundation
 
 extension Date {
     var year: Int {
-        return Calendar.current.component(.year, from: self)
+        return Calendar(identifier: .gregorian).component(.year, from: self)
     }
     
     var month: Int {
-         return Calendar.current.component(.month, from: self)
+         return Calendar(identifier: .gregorian).component(.month, from: self)
     }
     
     var day: Int {
-         return Calendar.current.component(.day, from: self)
+         return Calendar(identifier: .gregorian).component(.day, from: self)
     }
     
     var weekday: Int {
-        return Calendar.current.component(.weekday, from: self)
+        return Calendar(identifier: .gregorian).component(.weekday, from: self)
     }
     
     var monthName: String {
