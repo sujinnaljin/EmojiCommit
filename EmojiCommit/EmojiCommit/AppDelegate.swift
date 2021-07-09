d336a453dcd759b39477a80f4c5f03df9dd4723e
sujin <rkdthd1234@naver.com>
2021-07-09 21:05:36 +0900

    Add FirebaseAnalytics / FirebaseCrashlytics with SPM

--- EmojiCommit/EmojiCommit/AppDelegate.swift
+++ EmojiCommit/EmojiCommit/AppDelegate.swift
@@ -6,12 +6,13 @@
 //
 
 import UIKit
-
+import Firebase
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
