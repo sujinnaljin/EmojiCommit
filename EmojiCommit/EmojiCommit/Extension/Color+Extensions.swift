a74d5ee815c377d196bec829bedefe57b07cdb1c
sujin <rkdthd1234@naver.com>
2021-07-09 18:10:48 +0900

    Make github green color set

---
+++ EmojiCommit/EmojiCommit/Extension/Color+Extensions.swift
@@ -0,0 +1,17 @@
+//
+//  Color+Extensions.swift
+//  EmojiCommit
+//
+//  Created by 강수진 on 2021/07/09.
+//
+
+import SwiftUI
+
+extension Color {
+    static let greenGradeZero = Color("greenGradeZero")
+    static let greenGradeOne = Color("greenGradeOne")
+    static let greenGradeTwo = Color("greenGradeTwo")
+    static let greenGradeThree = Color("greenGradeThree")
+    static let greenGradeFour = Color("greenGradeFour")
+    
+}
