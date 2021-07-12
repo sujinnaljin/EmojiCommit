254f25181626254912e1ab238b2faed754894ee5
sujin <rkdthd1234@naver.com>
2021-07-13 00:07:58 +0900

    Set run script environment to make localized file from Localizable
    ## Description
    - string extension의 localized 는 run script에 의해 사용되고,  I18N은 run script의 실행결과가 write 됨

---
+++ EmojiCommit/EmojiCommit/Common/I18N.swift
@@ -0,0 +1,26 @@
+struct I18N {
+
+    //ThemeView
+    static let selectTheme = "selectTheme".localized
+    static let defaultString = "defaultString".localized
+    static let halloween = "halloween".localized
+    static let save = "save".localized
+
+    //LoginView
+    static let enterGithubId = "enterGithubId".localized
+    static let githubIdPlaceHolder = "githubIdPlaceHolder".localized
+
+    //CommitView
+    static let changeId = "changeId".localized
+    static let changeTheme = "changeTheme".localized
+    static let monday = "monday".localized
+    static let tuesday = "tuesday".localized
+    static let wednesday = "wednesday".localized
+    static let thursday = "thursday".localized
+    static let friday = "friday".localized
+    static let saturday = "saturday".localized
+    static let sunday = "sunday".localized
+
+    //Widget
+    static let widgetIntroduction = "widgetIntroduction".localized
+}
--- EmojiCommit/EmojiCommit/Extension/String+Extensions.swift
+++ EmojiCommit/EmojiCommit/Extension/String+Extensions.swift
@@ -22,4 +22,8 @@ extension String {
             return nil
         }
     }
+    
+    var localized: String {
+        return NSLocalizedString(self, comment: "")
+    }
 }
