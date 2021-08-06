3172e15213c1ba4fd32af1f05ec8447941bb1ed0
sujin <rkdthd1234@naver.com>
2021-08-06 18:00:56 +0900

    Make request that can decode data

--- EmojiCommit/EmojiCommit/Service/APIService.swift
+++ EmojiCommit/EmojiCommit/Service/APIService.swift
@@ -56,4 +56,50 @@ final class APIService: Requestable {
         
         return result
     }
+    
+    func request<T>(_ request: NetworkRequest) -> AnyPublisher<T, APIServiceError> where T : Decodable {
+        let sessionConfig = URLSessionConfiguration.default
+        sessionConfig.timeoutIntervalForRequest = TimeInterval(request.requestTimeOut ?? requestTimeOut)
+        
+        guard let encodedUrl = request.url.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed),
+              let url = URL(string: encodedUrl) else {
+            return AnyPublisher(Fail<T, APIServiceError>(error: .internetError))
+        }
+        
+        let result = URLSession.shared
+            .dataTaskPublisher(for: request.buildURLRequest(with: url))
+            .tryMap { element -> Data in
+                if let httpResponse = element.response as? HTTPURLResponse {
+                    let statusCode = httpResponse.statusCode
+                    switch HttpStatusCode(rawValue: statusCode) {
+                    case .success:
+                        break
+                    case .notFoundError:
+                        throw APIServiceError.clientError
+                    case .internalServerError:
+                        throw APIServiceError.serverError
+                    default:
+                        break
+                    }
+                }
+                return element.data
+            }
+            .decode(type: T.self, decoder: JSONDecoder())
+            .mapError({ error -> APIServiceError in
+                switch error {
+                case let error as APIServiceError:
+                    return error
+                case let error as NSError:
+                    if URLError.Code(rawValue: error.code) == .notConnectedToInternet {
+                        return APIServiceError.internetError
+                    } else {
+                        return APIServiceError.unknowError(error.localizedDescription)
+                    }
+                }
+            })
+            .receive(on: RunLoop.main)
+            .eraseToAnyPublisher()
+        
+        return result
+    }
 }
--- EmojiCommit/EmojiCommit/Service/Requestable.swift
+++ EmojiCommit/EmojiCommit/Service/Requestable.swift
@@ -10,5 +10,7 @@ import Combine
 
 protocol Requestable {
     var requestTimeOut: Float { get }
+    // 무엇을 호출하는지 결정하는건 request를 호출 했을 때 결과 값으로 받는 데이터 타입이 어떤 거냐에 따라서 달라짐
     func request(_ request: NetworkRequest) -> AnyPublisher<Data, APIServiceError>
+    func request<T: Decodable>(_ request: NetworkRequest) -> AnyPublisher<T, APIServiceError>
 }
